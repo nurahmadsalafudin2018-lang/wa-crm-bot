5e3b1ac903c3bddc21cb5c2eb9118aa0e65c5d4d
nurahmadsalafudin2018-lang <nurahmadsalafudin2018@gmail.com>
2026-06-11 17:38:16 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>res.json({status:'ok'}));
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>console.log('Running on',PORT));
